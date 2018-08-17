698344a3685e1b858a0c5b20c7e6adb3446c179b
Deepanshu Sinha <107sinha@gmail.com>
2018-08-18 00:28:45 +0530

    fizzbuzz game implemented

---
+++ fizzbuss.py
@@ -0,0 +1,21 @@
+"""
+    Fizzbuss game
+"""
+
+
+def fizBuzz(value):
+    for i in range(1, 51):
+        if(i % 3 == 0 and i % 5 == 0):
+            print(str(i)+" = FizzBuzz")
+        else:
+            if(i % 3 == 0):
+                print(str(i)+" = Fizz")
+            else:
+                if(i % 5 == 0):
+                    print(str(i)+" = Buzz")
+                else:
+                    print(str(i))
+
+
+# Solution
+fizBuzz(51)
