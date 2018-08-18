81c24c3757c664c20981faba5ddaa6ceb2cdefd0
Deepanshu Sinha <107sinha@gmail.com>
2018-08-18 09:59:25 +0530

    fizzbuzz solution added

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,7 +4,7 @@
 
 
 def fizBuzz(value):
-    for i in range(1, 51):
+    for i in range(1, value):
         if(i % 3 == 0 and i % 5 == 0):
             print(str(i)+" = FizzBuzz")
         else:
@@ -17,5 +17,106 @@ def fizBuzz(value):
                     print(str(i))
 
 
-# Solution
 fizBuzz(51)
+
+# Solution
+# 1
+# 2
+# 3 = Fizz
+# 4
+# 5 = Buzz
+# 6 = Fizz
+# 7
+# 8
+# 9 = Fizz
+# 10 = Buzz
+# 11
+# 12 = Fizz
+# 13
+# 14
+# 15 = FizzBuzz
+# 16
+# 17
+# 18 = Fizz
+# 19
+# 20 = Buzz
+# 21 = Fizz
+# 22
+# 23
+# 24 = Fizz
+# 25 = Buzz
+# 26
+# 27 = Fizz
+# 28
+# 29
+# 30 = FizzBuzz
+# 31
+# 32
+# 33 = Fizz
+# 34
+# 35 = Buzz
+# 36 = Fizz
+# 37
+# 38
+# 39 = Fizz
+# 40 = Buzz
+# 41
+# 42 = Fizz
+# 43
+# 44
+# 45 = FizzBuzz
+# 46
+# 47
+# 48 = Fizz
+# 49
+# 50 = Buzz
+# 1
+# 2
+# 3 = Fizz
+# 4
+# 5 = Buzz
+# 6 = Fizz
+# 7
+# 8
+# 9 = Fizz
+# 10 = Buzz
+# 11
+# 12 = Fizz
+# 13
+# 14
+# 15 = FizzBuzz
+# 16
+# 17
+# 18 = Fizz
+# 19
+# 20 = Buzz
+# 21 = Fizz
+# 22
+# 23
+# 24 = Fizz
+# 25 = Buzz
+# 26
+# 27 = Fizz
+# 28
+# 29
+# 30 = FizzBuzz
+# 31
+# 32
+# 33 = Fizz
+# 34
+# 35 = Buzz
+# 36 = Fizz
+# 37
+# 38
+# 39 = Fizz
+# 40 = Buzz
+# 41
+# 42 = Fizz
+# 43
+# 44
+# 45 = FizzBuzz
+# 46
+# 47
+# 48 = Fizz
+# 49
+# 50 = Buzz
