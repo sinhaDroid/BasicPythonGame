68e3366f9353d66296d25c8c3e93b438f7e5c79a
Deepanshu Sinha <107sinha@gmail.com>
2018-09-18 22:32:22 +0530

    duplicate added

---
+++ duplicate/duplicate.py
@@ -0,0 +1,17 @@
+def removeDuplicate(li):
+    newli = []
+    seen = set()
+    for item in li:
+        if item not in seen:
+            seen.add(item)
+            newli.append(item)
+
+    return newli
+
+
+li = []
+li = list(map(int, input().split()))
+x = removeDuplicate(li)
+
+for i in x:
+    print(i, end=" ")
