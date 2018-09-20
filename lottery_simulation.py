68fae7f3208e0cc0f138d5913717d9a92f772d6d
Deepanshu Sinha <107sinha@gmail.com>
2018-09-20 18:21:17 +0530

    lottery simulation with simulation working

--- lottery_simulation.py
+++ lottery_simulation.py
@@ -1,8 +1,13 @@
 import random
+import matplotlib.pyplot as plt
 
 account = 0
+x = []
+y = []
 
 for i in range(365):
+    x.append(i + 1)
+
     bet = random.randint(1, 10)
     lucky_draw = random.randint(1, 10)
 
@@ -10,5 +15,9 @@ for i in range(365):
         account = account + 900 - 100
     else:
         account = account - 100
+    y.append(account)
 
 print("Amount in your game account: ", account)
+
+plt.plot(x, y)
+plt.show()
