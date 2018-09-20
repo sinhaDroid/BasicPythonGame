51d6de6f8d14cc37bb4b94a0ed898fa8b9b400bb
Deepanshu Sinha <107sinha@gmail.com>
2018-09-21 02:40:40 +0530

    pattern modified working

--- pattern/pattern.py
+++ pattern/pattern.py
@@ -1,12 +1,32 @@
 def pattern(n):
-    for i in range(1, n+1):
-        for j in range(1, i+1):
-            if j == i:
-                print(j, end='')
+
+    # initialising starting number
+    num = 1
+
+    # outer loop to handle number of rows
+    for i in range(0, n):
+
+        # re assigning num
+        num = 1
+
+        # inner loop to handle number of columns
+        # values changing acc. to outer loop
+        for j in range(0, i+1):
+
+                # printing number
+            if(j == i):
+                print(num, end="")
             else:
-                print(j, end=' ')
-        print()
+                print(num, end=" ")
+
+            # incrementing number at each column
+            num = num + 1
+
+        # ending line after each row
+        if(i != n-1):
+            print()
 
 
+# Driver code
 n = int(input())
 pattern(n)
