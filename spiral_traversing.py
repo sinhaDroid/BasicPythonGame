26129dbc207be4fd5c98babfee2039431f278cb2
Deepanshu Sinha <107sinha@gmail.com>
2018-09-20 16:41:02 +0530

    spiral traversing turtle graphics added

--- spiral_traversing.py
+++ spiral_traversing.py
@@ -1,7 +1,22 @@
+import turtle
 
-def spiral(m, n, a):
+turtle.bgcolor("black")
+seurat = turtle.Turtle()
+
+width = 5
+height = 7
+
+dot_distance = 25
+
+seurat.setpos(-250, 250)
+
+
+def spiral(m, n):
     k = 0
     l = 0
+    f = 0
+
+    seurat.color("white")
 
     '''
     k = index of starting row
@@ -10,38 +25,41 @@ def spiral(m, n, a):
 
     while(k < m and l < n):
 
+        if(f == 1):
+            seurat.right(90)
+
         # Printing the first row from the remaining rows
         for i in range(l, n):
-            print(a[k][i], end=" ")
+            seurat.forward(dot_distance)
+            # print(a[k][i], end = " ")
 
         k += 1
+        f = 1
 
+        seurat.right(90)
         # Printing the last column from the remaining column
         for i in range(k, n):
-            print(a[i][n - 1], end=" ")
+            seurat.forward(dot_distance)
+            # print(a[i][n-1], end = " ")
 
         n -= 1
+        seurat.right(90)
 
         if(k < m):
             # Printing the last row from remaining rows
             for i in range(n-1, l-1, -1):
-                print(a[m-1][i], end=" ")
+                seurat.forward(dot_distance)
+                # print(a[m-1][i], end=" ")
             m -= 1
 
+        seurat.right(90)
         if(l < n):
             # Printing the first column from the remaining column
             for i in range(m-1, k-1, -1):
-                print(a[i][l], end=" ")
+                seurat.forward(dot_distance)
+                # print(a[i][l], end=" ")
             l += 1
 
 
-a = []
-count = 1
-for i in range(4):
-    l = []
-    for j in range(4):
-        l.append(count)
-        count += 1
-    a.append(l)
-
-spiral(4, 4, a)
+spiral(20, 20)
+turtle.done()
