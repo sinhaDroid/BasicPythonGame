ca1886d089e6975c8868d4a604f27e4df9d9d8be
Deepanshu Sinha <107sinha@gmail.com>
2018-10-03 23:27:58 +0530

    faded palindromes method added to calulcate smallest palindrome

---
+++ smallest-palindromes/smallest_palindromes.py
@@ -0,0 +1,18 @@
+#
+# Faded Palindrome
+# check & construct a lexicographically smallest palindrome by replacing the '.' with suitable characters
+#
+def fadedPalindrome(s):
+    hlen = len(s)
+    slist = []
+    slist.extend(s)
+    for i in range(hlen//2):   # check, if given characters fulfill palindrome condition
+        if slist[i] != '.' and slist[hlen-1-i] != '.' and slist[i] != slist[hlen-1-i]: 
+            print(-1)
+            return
+    for i in range(hlen):      
+        if slist[i] == '.' and slist[hlen-1-i] != '.': # fill '.' with character, which can be found symmetrical
+            slist[i] = slist[hlen-1-i]
+        elif slist[i] == '.':                          # fill '.' with 'a'
+            slist[i] = 'a'
+    print("".join(slist))
