f80cafe7e9bbf1cd83e6233e11452e8c20e3f800
Deepanshu Sinha <107sinha@gmail.com>
2018-10-03 23:29:00 +0530

    reformat the code in smallest palindrome

--- smallest-palindromes/smallest_palindromes.py
+++ smallest-palindromes/smallest_palindromes.py
@@ -2,23 +2,27 @@
 # Faded Palindrome
 # check & construct a lexicographically smallest palindrome by replacing the '.' with suitable characters
 #
+
+
 def fadedPalindrome(s):
     hlen = len(s)
     slist = []
     slist.extend(s)
     for i in range(hlen//2):   # check, if given characters fulfill palindrome condition
-        if slist[i] != '.' and slist[hlen-1-i] != '.' and slist[i] != slist[hlen-1-i]: 
+        if slist[i] != '.' and slist[hlen-1-i] != '.' and slist[i] != slist[hlen-1-i]:
             print(-1)
             return
-    for i in range(hlen):      
-        if slist[i] == '.' and slist[hlen-1-i] != '.': # fill '.' with character, which can be found symmetrical
+    for i in range(hlen):
+        # fill '.' with character, which can be found symmetrical
+        if slist[i] == '.' and slist[hlen-1-i] != '.':
             slist[i] = slist[hlen-1-i]
         elif slist[i] == '.':                          # fill '.' with 'a'
             slist[i] = 'a'
     print("".join(slist))
 
+
 #
 # demo
 #
 fadedPalindrome('a..ba...ca')  # output: acabaabaca
-fadedPalindrome('a..ba...cb')  # output: -1 
+fadedPalindrome('a..ba...cb')  # output: -1
